9f02b8706a908a82922aef699fcfb21c1874e2f1
Davi Silva <davisilvaphoto@gmail.com>
2026-08-17 17:55:37 -0300

    feat: add custom logger

--- main.go
+++ main.go
@@ -1,7 +1,23 @@
 package main
 
-import "github.com/davi1985/gopportunities/router"
+import (
+	"github.com/davi1985/gopportunities/config"
+	"github.com/davi1985/gopportunities/router"
+)
+
+var (
+	logger *config.Logger
+)
 
 func main() {
+	logger = config.GetLogger("main")
+	// Init configs
+	err := config.Init()
+
+	if err != nil {
+		logger.Errorf("config initialization error: %v", err)
+		return
+	}
+
 	router.Initialize()
 }

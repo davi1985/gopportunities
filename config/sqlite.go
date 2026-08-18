9da1e79113eaf285e500df7c65fc54ff09a868f6
Davi Silva <davisilvaphoto@gmail.com>
2026-08-18 07:34:27 -0300

    feat: add database handler

---
+++ config/sqlite.go
@@ -0,0 +1,49 @@
+package config
+
+import (
+	"os"
+
+	"github.com/davi1985/gopportunities/schemas"
+	"gorm.io/driver/sqlite"
+	"gorm.io/gorm"
+)
+
+func InitSQLite() (*gorm.DB, error) {
+	logger := GetLogger("sqlite")
+	dbPath := "./db/main.db"
+
+	// Check if the database file exists
+	_, err := os.Stat(dbPath)
+	if os.IsNotExist(err) {
+		logger.Info("database file not found, creating...")
+		err = os.MkdirAll("./db", os.ModePerm)
+		if err != nil {
+			return nil, err
+		}
+
+		file, err := os.Create(dbPath)
+		if err != nil {
+			return nil, err
+		}
+		file.Close()
+	}
+
+	// Create DB and connect
+	db, err := gorm.Open(
+		sqlite.Open(dbPath),
+		&gorm.Config{},
+	)
+	if err != nil {
+		logger.Errorf("sqlite opening error: %v", err)
+		return nil, err
+	}
+
+	// Migrate the Schema
+	err = db.AutoMigrate(&schemas.Opening{})
+	if err != nil {
+		logger.Errorf("sqlite AutoMigration error: %v", err)
+		return nil, err
+	}
+	// Return the DB
+	return db, nil
+}
